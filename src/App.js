a839dd15ec1e278830b8d9bd8c4291ea9a824696
Lucas <lucasvidigal3@gmail.com>
2020-02-19 21:35:30 -0300

    Create buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import ButtonsWatchs from './components/ButtonsWatchs';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <ButtonsWatchs></ButtonsWatchs>
     </div>
   );
 }
---
+++ src/components/ButtonsWatchs/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+const ButtonsWatchs = () => (
+
+    <div className="watchs">
+        <button id="pomodoro">Pomodoro</button>
+        <button id="break">Break</button>
+    </div>
+
+);
+
+export default ButtonsWatchs;
